e32b9e4fca98a6737fcb892900f295d775b2c0f4
Domcegas <domasstanislauskas@gmail.com>
2026-03-06 17:27:14 +0200

    Sukurti frontend griaučiai ir pagrindinis puslapis

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/MainPage.css';
+
+function MainPage() {
+  return (
+    <div className="main-container">
+      <h1>Foto-Puzlė</h1>
+      <p>
+        Užsisakykite savo unikalią dėlionę iš bet kurios nuotraukos!  
+        Tai puiki dovana arba linksmas projektas šeimai ir draugams.
+      </p>
+    </div>
+  );
+}
+
+export default MainPage;
